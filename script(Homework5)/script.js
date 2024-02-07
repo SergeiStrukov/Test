7c304d0edbd3ddf1e39bb4099b9421e1e65d4224
SergeiStrukov <156689541+SergeiStrukov@users.noreply.github.com>
2024-02-07 07:31:32 +0400

    Add files via upload

---
+++ script(Homework5)/script.js
@@ -0,0 +1,42 @@
+// Задача1
+
+// let a = +prompt('Введите пожалуйста число')
+// console.log(a*0.1)
+
+// Задача2 
+
+// let a = +prompt('Введите пожалуйста 1 число')
+// let b = +prompt('Введите пожалуйста 2 число')
+// if (a<b) {
+//     console.log(a)
+// }
+// else if (a==b) {
+// } 
+// else {
+//     console.log(b)
+// }
+
+// Задача3
+
+// let a = +prompt('Введите пожалуйста число')
+// if (a<100) {
+//     console.log('Число меньше 100')
+// }
+// else if (a==100) {
+//     console.log('Число равно 100')
+// }
+// else {
+//     console.log('Число больше 100')
+// }
+
+// Задача4
+
+const name = prompt("Ваше имя:")
+let age = prompt("Ваш возраст:")
+
+if (age < 18) {
+    console.log(`Hi, ${name}`)
+}
+else {
+    console.log(`Hello, ${name}`)
+}
